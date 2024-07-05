672f2fb34c1a7ccd20781579fa5cb1dc1c585661
antares <antares@alyr.dev>
2024-07-06 01:45:52 +0800

    Fix: type checking in vscode fails when typing_extensions.py exists

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let
   using_python = pkgs.python312;
   # import required python packages
   required_python_packages = import ./py_requirements.nix pkgs;
-  #
+  # define the nix-pyenv directory
   nix_pyenv_directory = ".nix-pyenv";
   pyenv = using_python.withPackages required_python_packages;
 in
@@ -15,6 +15,7 @@ pkgs.mkShell {
   shellHook = ''
     cd ${builtins.toString ./.}
 
+    # ensure the nix-pyenv directory exists
     if [[ ! -d ${nix_pyenv_directory} ]]; then mkdir ${nix_pyenv_directory}; fi
     if [[ ! -d ${nix_pyenv_directory}/lib ]]; then mkdir ${nix_pyenv_directory}/lib; fi
     if [[ ! -d ${nix_pyenv_directory}/bin ]]; then mkdir ${nix_pyenv_directory}/bin; fi
@@ -29,14 +30,30 @@ pkgs.mkShell {
         ln -s "$target_path" "$link_path"
     }
 
+    # creating python library symlinks
     for file in ${pyenv}/${using_python.sitePackages}/*; do
-        ensure_symlink ${nix_pyenv_directory}/lib/$(basename $file) $file
+        basefile=$(basename $file)
+        if [ -d "$file" ]; then
+            if [[ "$basefile" != *dist-info && "$basefile" != __pycache__ ]]; then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        else
+            # the typing_extensions.py will make the vscode type checker not working!
+            if [[ $file == *.so ]] || ([[ $file == *.py ]] && [[ $file != typing_extensions.py ]]); then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        fi
     done
     for file in ${nix_pyenv_directory}/lib/*; do
         if [[ -L "$file" ]] && [[ "$(dirname $(readlink "$file"))" != "${pyenv}/${using_python.sitePackages}" ]]; then
             rm -f "$file"
         fi
     done
+
+    # ensure the typing_extensions.py is not in the lib directory
+    rm ${nix_pyenv_directory}/lib/typing_extensions.py > /dev/null 2>&1
+
+    # add python executable to the bin directory
     ensure_symlink ${nix_pyenv_directory}/bin/python ${pyenv}/bin/python
     export PATH=${nix_pyenv_directory}/bin:$PATH
   '';
